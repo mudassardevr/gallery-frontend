89fa54befb7b9486139ded8257763eee0ae52da5
mudassardevr <asuspulsar3@gmail.com>
2026-03-18 21:17:05 +0530

    toast added

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -4,6 +4,7 @@ import Navbar from "../ui/Navbar";
 import BottomNav from "../ui/BottomNav";
 import { deleteImageAPI } from "../../Services/imageService";
 import DeleteModal from "../common/DeleteModal";
+import { toast } from "react-toastify";
 
 
 function MainLayout() {
@@ -13,20 +14,37 @@ function MainLayout() {
   const [selectedId, setSelectedId] = useState(null);
 
   const handleDeleteClick = (id) => {
-  setSelectedId(id);
-  setShowModal(true);
-};
+    setSelectedId(id);
+    setShowModal(true);
+  };
 
-const handleConfirmDelete = async () => {
-  try {
-    await deleteImageAPI(selectedId);
-    setRefresh(prev => !prev);
-  } finally {
-    setShowModal(false);
-    setSelectedId(null);
-    setActiveId(null);
-  }
-};
+  const handleConfirmDelete = async () => {
+      const toastId = toast.loading("Deleting...");
+    try {
+      await deleteImageAPI(selectedId);
+    
+      toast.update(toastId, {
+      render: "Image deleted successfully ",
+      type: "success",
+      isLoading: false,
+      autoClose: 2000,
+    });
+
+      setRefresh((prev) => !prev);
+    } catch(error){
+       toast.update(toastId, {
+      render: "Delete failed ",
+      type: "error",
+      isLoading: false,
+      autoClose: 2000,
+    });
+    }
+    finally {
+      setShowModal(false);
+      setSelectedId(null);
+      setActiveId(null);
+    }
+  };
   // const handleDeleteImage = async (id) => {
   //   await deleteImageAPI(id);
   //   setActiveId(null);
@@ -36,25 +54,25 @@ const handleConfirmDelete = async () => {
 
   return (
     <>
-     <DeleteModal
-  isOpen={showModal}
-  onClose={() => setShowModal(false)}
-  onConfirm={handleConfirmDelete}
-/>
-   
-    <div className="min-h-screen  bg-linear-to-r from-[#EEAECA] to-[#94BBE9] ">
-      <Navbar activeId={activeId} handleDeleteClick={handleDeleteClick} />
+      <DeleteModal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        onConfirm={handleConfirmDelete}
+      />
 
-      {/* Page component */}
-      <div className="pb-24">
-        <Outlet
-          context={{ activeId, setActiveId, handleDeleteClick, refresh }}
-        />
+      <div className="min-h-screen  bg-linear-to-r from-[#EEAECA] to-[#94BBE9] ">
+        <Navbar activeId={activeId} handleDeleteClick={handleDeleteClick} />
+
+        {/* Page component */}
+        <div className="pb-24">
+          <Outlet
+            context={{ activeId, setActiveId, handleDeleteClick, refresh }}
+          />
+        </div>
+
+        <BottomNav />
       </div>
-
-      <BottomNav />
-    </div>
-     </>
+    </>
   );
 }
 
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import deleteIcon from "../../assets/delete-icon.svg"
+import deleteIcon from "../../assets/delete-icon.svg";
 
-function Navbar({activeId ,  handleDeleteClick}) {
+function Navbar({ activeId, handleDeleteClick }) {
   return (
     <div className="p-4 sticky top-0 bg-white/30 backdrop-blur-md shadow-xl flex justify-between items-center z-50">
       <h1 className="flex items-center font-bold ">My Gallery</h1>
       {activeId && (
         <button
-              onClick={() => handleDeleteClick(activeId)}
-              className="w-5 h-5 md:hidden"
-            >
-              <img src={deleteIcon} alt="3" />
-            </button>
+          title="Delete selected image"
+          onClick={() => handleDeleteClick(activeId)}
+          className="w-5 h-5 md:hidden"
+        >
+          <img src={deleteIcon} alt="delete icon" />
+        </button>
       )}
-
-
     </div>
   );
 }
