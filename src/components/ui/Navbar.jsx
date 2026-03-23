bb8c04b2516c972fe887841a854eab138e00a23d
mudassardevr <asuspulsar3@gmail.com>
2026-03-23 18:19:40 +0530

    logo adding

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import deleteIcon from "../../assets/delete-icon.svg";
+import mLogo from "../../assets/M-logo.svg"
 
 function Navbar({ activeId, handleDeleteClick }) {
   return (
     <div className="p-4 sticky top-0 bg-white/30 backdrop-blur-md shadow-xl flex justify-between items-center z-50">
-      <h1 className="flex items-center font-bold ">My Gallery</h1>
+      <img src={mLogo} alt="m-logo" className="w-5 h-5"/>
+      <span className="flex items-center font-bold ">Gallery</span>
       {activeId && (
         <button
           title="Delete selected image"
