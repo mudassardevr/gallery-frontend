16e1157240286d64e5ae7f3b8a2dea2ce1d4d078
mudassardevr <asuspulsar3@gmail.com>
2026-03-24 19:00:15 +0530

    settings update

--- src/components/pages/Settings.jsx
+++ src/components/pages/Settings.jsx
@@ -2,12 +2,9 @@ import React, { useState } from "react";
 
 function Settings() {
   return (
-    <div className="p-4 max-w-md mx-auto text-black">
-      <h2 className="text-2xl font-bold mb-4">Settings</h2>
-
-      <div className="text-center py-10 text-gray-500">
-        <p className="mt-2">Coming Soon</p>
-      </div>
+    <div className="text-center py-10 text-gray-500">
+      <h2 className="text-xl font-semibold">Settings</h2>
+      <p className="mt-2">Coming Soon</p>
     </div>
   );
 }
